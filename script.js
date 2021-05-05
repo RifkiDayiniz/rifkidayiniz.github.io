c157b422e5354c58366617c3fd3a78dd2a4b34c5
RifkiDayiniz <83727278+RifkiDayiniz@users.noreply.github.com>
2021-05-05 23:19:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var ism = document.getElementById("ism")
+ism.style.display='none'
+document.getElementById("blg").style.display='none'
+
+setTimeout(function() {
+    ism.style.display='block'
+    document.getElementById("ism").classList.add("animate__animated")
+    document.getElementById("ism").classList.add("animate__slideInDown")
+}, 2000)
+
+setTimeout(function() {
+    document.getElementById("blg").style.display='block'
+    document.getElementById("blg").classList.add("animate__animated")
+    document.getElementById("blg").classList.add("animate__slideInDown")
+}, 4000)
+
+console.log("niye burdasın çakal seni :)")
