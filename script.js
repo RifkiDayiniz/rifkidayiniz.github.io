e74a171ebfce7019988d3a6ea2f7462024a0b6f0
RifkiDayiniz <83727278+RifkiDayiniz@users.noreply.github.com>
2021-05-06 08:34:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ document.getElementById("blg").style.display='none'
 setTimeout(function() {
     ism.style.display='block'
     document.getElementById("ism").classList.add("animate__animated")
-    document.getElementById("ism").classList.add("animate__slideInDown")
+    document.getElementById("ism").classList.add("animate__slideInRight")
 }, 2000)
 
 setTimeout(function() {
     document.getElementById("blg").style.display='block'
     document.getElementById("blg").classList.add("animate__animated")
-    document.getElementById("blg").classList.add("animate__slideInDown")
+    document.getElementById("blg").classList.add("animate__slideInRight")
 }, 4000)
 
 console.log("niye burdasın çakal seni :)")
