7e83420f735ee2d21f1fa53acdb1839b99adffa3
RifkiDayiniz <83727278+RifkiDayiniz@users.noreply.github.com>
2021-05-28 13:49:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,42 @@ function sos() {
   bilgi.classList.remove("animate__slideInRight")
   projeler.classList.remove("animate__slideInRight")
 }
+
+var projeler = document.getElementById("projelerim")
+var bilgi = document.getElementById("bilgi")
+var sosyal = document.getElementById("sosyal")
+projeler.style.display = "none"
+sosyal.style.display = "none"
+
+function hak() {
+  sosyal.style.display = "none"
+  projeler.style.display = "none"
+  bilgi.style.display = "block"
+  bilgi.classList.add("animate__slideInRight")
+  projeler.classList.remove("animate__slideInRight")
+  sosyal.classList.remove("animate__slideInRight")
+}
+
+function pro() {
+  bilgi.style.display = "none"
+  sosyal.style.display = "none"
+  projeler.style.display = "block"
+  projeler.classList.add("animate__slideInRight")
+  bilgi.classList.remove("animate__slideInRight")
+  sosyal.classList.remove("animate__slideInRight")
+}
+
+function sos() {
+  bilgi.style.display = "none"
+  projeler.style.display = "none"
+  sosyal.style.display = "block"
+  sosyal.classList.add("animate__slideInRight")
+  bilgi.classList.remove("animate__slideInRight")
+  projeler.classList.remove("animate__slideInRight")
+}
+
+function egl() {
+    bilgi.contentEditable = true
+    projeler.contentEditable = true
+    sosyal.contentEditable = true
+}
